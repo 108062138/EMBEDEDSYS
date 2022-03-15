d90abed0cc36e1862cca3da05c2bdae020215877
108062138 <poyuwu17@gmail.com>
2022-03-15 11:07:17 +0800

    led change bright

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 default:
-	avr-g++ -L C:\Users\USER\Desktop\arduino_build\arduino -I C:\Users\USER\Desktop\arduino_build\arduino -Wall -DF_CPU=16000000UL -Os -mmcu=atmega328p -o blink_led.exe blink_led.c -larduino
-	avr-objcopy -O ihex -R .eeprom blink_led.exe blink_led.hex
+	avr-g++ -L C:\Users\USER\Desktop\arduino_build\arduino -I C:\Users\USER\Desktop\arduino_build\arduino -Wall -DF_CPU=16000000UL -Os -mmcu=atmega328p -o changeBright.exe changeBright.c -larduino
+	avr-objcopy -O ihex -R .eeprom changeBright.exe changeBright.hex
 upload:
-	avrdude -c arduino -p m328p -b 57600 -P COM4 -U flash:w:blink_led.hex
+	avrdude -c arduino -p m328p -b 57600 -P COM4 -U flash:w:changeBright.hex
 all: 
 	default upload
--- servermoter.c
+++ servermoter.c
@@ -1,34 +1,34 @@
 //basic
-//#include <avr/io.h>
-//#include <util/delay.h>
-//int main(void) {
-//    DDRB |= 1 << PB1;
-//    TCCR1A |= (1 << WGM11) | (1 << COM1A1);
-//    TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11);
-//    ICR1 = 39999;
-//    int offset = 400;
-//    while(1) {
-//        OCR1A = 1599 + offset;
-//        _delay_ms(1000);
-//        OCR1A = 1099 - offset;
-//        _delay_ms(1000);
-//    }
-//    return 0;
-//}
-
-//bonus
 #include <avr/io.h>
 #include <util/delay.h>
 int main(void) {
-    DDRB |= 1 << PB3;
-    TCCR2A |= (1 << WGM20) | (1 << WGM21)| (1 << COM2A1);
-    TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
-    int offset = 30;
+    DDRB |= 1 << PB1;
+    TCCR1A |= (1 << WGM11) | (1 << COM1A1);
+    TCCR1B |= (1 << WGM12) | (1 << WGM13) | (1 << CS11);
+    ICR1 = 39999;
+    int offset = 400;
     while(1) {
-        OCR2A = 45 + offset;
-        _delay_ms(500);
-        OCR2A = 45 - offset;
-        _delay_ms(500);
+        OCR1A = 1599 + offset;
+        _delay_ms(1000);
+        OCR1A = 1099 - offset;
+        _delay_ms(1000);
     }
     return 0;
 }
+
+//bonus
+//#include <avr/io.h>
+//#include <util/delay.h>
+//int main(void) {
+//    DDRB |= 1 << PB3;
+//    TCCR2A |= (1 << WGM20) | (1 << WGM21)| (1 << COM2A1);
+//    TCCR2B |= (1 << CS22) | (1 << CS21) | (1 << CS20);
+//    int offset = 30;
+//    while(1) {
+//        OCR2A = 45 + offset;
+//        _delay_ms(500);
+//        OCR2A = 45 - offset;
+//        _delay_ms(500);
+//    }
+//    return 0;
+//}
